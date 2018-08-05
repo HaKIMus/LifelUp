2c3c9494a88d7a675071a7faabe701979e68672b
HaKIMus <webdevhakim@gmail.com>
2018-08-05 07:48:56 +0200

    I forgot about declaring a strict_types

--- spec/Domain/Quest/RewardSpec.php
+++ spec/Domain/Quest/RewardSpec.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace spec\App\Domain\Quest;
 
 use App\Domain\Quest\Reward;
--- src/Domain/Quest/Experience.php
+++ src/Domain/Quest/Experience.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Domain\Quest;
 
 use App\Domain\Exception\InvalidArgumentException;
